a97b6370d9e767f12c5d97cd8177c49c00a4af1f
Mathias Rasmussen <mathiasvr@gmail.com>
2016-06-30 23:18:22 +0200

    :up:

---
+++ README.md
@@ -0,0 +1,31 @@
+# mkv-subtitle-extractor [![npm][npm-img]][npm-url] [![dependencies][dep-img]][dep-url] [![license][lic-img]][lic-url]
+
+[npm-img]: https://img.shields.io/npm/v/mkv-subtitle-extractor.svg
+[npm-url]: https://www.npmjs.com/package/mkv-subtitle-extractor
+[dep-img]: https://david-dm.org/mathiasvr/mkv-subtitle-extractor.svg
+[dep-url]: https://david-dm.org/mathiasvr/mkv-subtitle-extractor
+[lic-img]: http://img.shields.io/:license-MIT-blue.svg
+[lic-url]: http://mvr.mit-license.org
+
+Extract subtitles from `.mkv` files.
+
+Currently only supports the `.srt` format.
+
+## install
+
+```
+npm install -g mkv-subtitle-extractor
+```
+
+## usage
+
+```
+mkv-subtitle-extractor file.mkv
+```
+
+All supported subtitle tracks are written to the same destination with non-overwriting (incremental) names.
+> file.srt, file.2.srt, file.eng.srt, file.ger.srt, file.ita.srt, ...
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const z = require('zero-fill')
+const fileExists = require('file-exists')
+const matroskaSubtitles = require('matroska-subtitles')
+
+if (process.argv.length < 3) {
+  exitWithUsage(0)
+}
+
+var mkvPath = process.argv[2]
+var file = fs.createReadStream(mkvPath)
+
+file.on('error', function (err) {
+  console.error('Error:', err.message + '\n')
+  exitWithUsage(1)
+})
+
+var dir = path.dirname(mkvPath)
+var name = path.basename(mkvPath, path.extname(mkvPath))
+
+var srtPath = function (language) {
+  var languageSuffix = language ? '.' + language : ''
+  return path.join(dir, name + languageSuffix + '.srt')
+}
+
+var tracks = new Map()
+var subs = matroskaSubtitles()
+
+subs.on('data', function (data) {
+  if (data[0] === 'new') {
+    var subtitlePath = srtPath(data[1].language)
+
+    // obtain unique filename (don't overwrite)
+    for (var i = 2; fileExists(subtitlePath); i++) {
+      subtitlePath = srtPath(data[1].language + i || i)
+    }
+
+    tracks.set(data[1].track, {
+      index: 1,
+      file: fs.createWriteStream(subtitlePath)
+    })
+  } else {
+    var track = tracks.get(data[0])
+    var sub = data[1]
+
+    // convert to srt format
+    track.file.write(`${track.index++}\r\n`)
+    track.file.write(`${msToTime(sub.time)} --> ${msToTime(sub.time + sub.duration)}\r\n`)
+    track.file.write(`${sub.text}\r\n\r\n`)
+  }
+})
+
+subs.on('end', function () {
+  if (tracks.size === 0) return console.log('No subtitle tracks found.')
+
+  tracks.forEach(function (track, i) {
+    track.file.end()
+    console.log(`Track ${z(2, i)} → ${track.file.path}`)
+  })
+})
+
+file.pipe(subs)
+
+// https://stackoverflow.com/questions/9763441/milliseconds-to-time-in-javascript
+function msToTime (s) {
+  var ms = s % 1000
+  s = (s - ms) / 1000
+  var secs = s % 60
+  s = (s - secs) / 60
+  var mins = s % 60
+  var hrs = (s - mins) / 60
+
+  return z(2, hrs) + ':' + z(2, mins) + ':' + z(2, secs) + ',' + z(3, ms)
+}
+
+function exitWithUsage (code) {
+  var pkg = require('./package.json')
+  console.log(`Version ${pkg.version}`)
+  console.log(`Usage: ${pkg.name} <file.mkv>`)
+  process.exit(code)
+}
